3e01a728cf456c8601158af326cbbb3329fe8164
Maddy Daigneau <mad1948@g.rit.edu>
2016-05-09 13:22:08 -0400

    Update IOControl.h

--- IOControl.h
+++ IOControl.h
@@ -8,17 +8,33 @@
 #ifndef IOCONTROL_H_
 #define IOCONTROL_H_
 
+enum AN7SEG { // DECIMAL POINT ACTIVE - binary or later
+	ZERO = 0x02,
+	ONE = 0x9E,
+	TWO = 0x28,
+	THREE = 0x0C,
+	FOUR = 0x98,
+	FIVE = 0x48,
+	SIX = 0x40,
+	SEVEN = 0x1E,
+	EIGHT = 0x00,
+	NINE = 0x08
+}
+
 class IOControl {
 public:
 
 	/*
 	 * Add I/O Registers and message_queue l8r
+	 * 
+	 * Also add functionality to write to 7 segment displays, LEDs
 	 */
 
 	IOControl();
 	~IOControl();
 
 	void receive();
+	
 
 };
 
